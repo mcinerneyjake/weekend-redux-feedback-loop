f6520c1d30181fd0a16ee7456821c410d0e6880b
Jake McInerney <jakemcinerney1@gmail.com>
2022-05-05 22:19:32 -0500

    added Feeling.jsx and rendered to DOM in App

---
+++ src/components/Feeling/Feeling.jsx
@@ -0,0 +1,14 @@
+function Feeling() {
+  return (
+    <div>
+      <h1>How are you feeling today?</h1>
+      <div>
+        <p>Feeling?</p>
+        <input type='number' />
+      </div>
+      <button>NEXT</button>
+    </div>
+  );
+}
+
+export default Feeling;
