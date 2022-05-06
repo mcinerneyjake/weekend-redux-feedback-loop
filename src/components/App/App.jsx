2e9858496b3324ff49fbd91c0cfbe4e6402443be
Jake McInerney <jakemcinerney1@gmail.com>
2022-05-05 22:27:30 -0500

    added Understanding.jsx and rendered on DOM via App

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import axios from 'axios';
 import './App.css';
+import Understanding from '../Understanding/Understanding';
 
 function App() {
-
   return (
     <div className='App'>
       <header className='App-header'>
         <h1 className='App-title'>Feedback!</h1>
         <h4>Don't forget it!</h4>
       </header>
+      <Understanding />
     </div>
   );
 }
---
+++ src/components/Understanding/Understanding.jsx
@@ -0,0 +1,14 @@
+function Understanding() {
+    return (
+      <div>
+        <h1>How well are you understanding the content?</h1>
+        <div>
+          <p>Understanding?</p>
+          <input type='number' />
+        </div>
+        <button>NEXT</button>
+      </div>
+    );
+  }
+  
+  export default Understanding;
