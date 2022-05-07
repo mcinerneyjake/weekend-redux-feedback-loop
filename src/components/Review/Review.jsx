2d7c64f27b55f536ff5f7b7e33b6980b62a3d1d4
Jake McInerney <jakemcinerney1@gmail.com>
2022-05-07 12:29:56 -0500

    added styling to Review.jsx

--- src/components/Review/Review.jsx
+++ src/components/Review/Review.jsx
@@ -1,7 +1,10 @@
 function Review() {
   return (
     <>
-      <h1>Review Your Feedback</h1>
+      <div className='App-header'>
+        <h1>Review</h1>
+        <h2>Please review your submissions.</h2>
+      </div>
       <h3>Feelings: </h3>
       <h3>Understanding: </h3>
       <h3>Support: </h3>
