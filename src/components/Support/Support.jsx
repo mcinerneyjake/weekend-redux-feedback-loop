a3a7bf09ab18b0b25d412e03ce592fb5abb68f63
Jake McInerney <jakemcinerney1@gmail.com>
2022-05-07 13:42:33 -0500

    connected Support component payload to Review component via Redux

--- src/components/Support/Support.jsx
+++ src/components/Support/Support.jsx
@@ -1,12 +1,35 @@
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+
 function Support() {
+  let dispatch = useDispatch();
+
+  const [support, setSupport] = useState(0);
+
+  const addSupport = (event) => {
+    event.preventDefault();
+
+    console.log('In addSupport:', support);
+
+    dispatch({
+      type: 'SET_SUPPORT',
+      payload: support,
+    });
+  };
+
   return (
     <div>
       <h1>How well are you being supported?</h1>
-      <div>
-        <p>Support?</p>
-        <input type='number' />
-      </div>
-      <button>NEXT</button>
+      <form onSubmit={addSupport}>
+        <p>Please pick a number between 5 and 1 (5 High, 1 Low):</p>
+        <input
+          type='number'
+          placeholder='Support'
+          value={support}
+          onChange={(event) => setSupport(event.target.value)}
+        />
+        <button>NEXT</button>
+      </form>
     </div>
   );
 }
