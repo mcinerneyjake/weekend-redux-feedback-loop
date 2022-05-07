37ca74788aaf10a2c72cb82b7b8399a651d5a683
Jake McInerney <jakemcinerney1@gmail.com>
2022-05-07 12:19:30 -0500

    Review component added and imported into App

---
+++ src/components/Review/Review.jsx
@@ -0,0 +1,13 @@
+function Review() {
+  return (
+    <>
+      <h1>Review Your Feedback</h1>
+      <h3>Feelings: </h3>
+      <h3>Understanding: </h3>
+      <h3>Support: </h3>
+      <h3>Comments: </h3>
+    </>
+  );
+}
+
+export default Review;
